594ce7c76b59bdcc3d667c68d86814c7dcacf433
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-02-24 14:54:37 -0500

    Improving the UI of the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -24,11 +25,78 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(
                       height: 30,
                     ),
+                    _LoginForm(),
                   ],
                 ),
               ),
+              const SizedBox(height: 50),
+              const Text(
+                'Create new account',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: mantain state reference of the form using the key.
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'user.name@gmail.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '******',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline,
+              ),
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              onPressed: () {
+                // TODO: login form
+              },
+            )
+          ],
         ),
       ),
     );
