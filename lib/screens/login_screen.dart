22b584a8f18378d0fb964267ab431c42295fa6ea
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-02-24 13:18:08 -0500

    Implementing the first part of the UI of the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
-      ),
+      body: AuthBackground(),
     );
   }
 }
---
+++ lib/widgets/auth_background.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  const AuthBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(
+        children: [
+          _PurpleBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.4,
+      decoration: _purpleBackground(),
+      child: Stack(
+        children: [
+          Positioned(top: 90, left: 30, child: _Bubbles()),
+          Positioned(top: -40, left: -30, child: _Bubbles()),
+          Positioned(top: -50, right: -30, child: _Bubbles()),
+          Positioned(bottom: -50, left: 10, child: _Bubbles()),
+          Positioned(bottom: 120, right: 20, child: _Bubbles()),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration _purpleBackground() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        colors: [
+          Color.fromRGBO(63, 63, 156, 1),
+          Color.fromRGBO(90, 70, 178, 1)
+        ],
+      ),
+    );
+  }
+}
+
+class _Bubbles extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: const Color.fromRGBO(255, 255, 255, 0.05),
+      ),
+    );
+  }
+}
